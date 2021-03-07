7ee1756f402a05560726891a5f6cb7052449c376
Green-Avocado <greenavocado@protonmail.com>
2021-03-07 15:31:01 -0800

    feat: added sample source file

---
+++ src/etwahl.cpp
@@ -0,0 +1,16 @@
+#include "RtMidi.h"
+
+int main() {
+    try
+    {
+        RtMidiIn midiin;
+    }
+    catch (RtMidiError &error)
+    {
+        // Handle the exception here
+        error.printMessage();
+    }
+
+    return 0;
+}
+
