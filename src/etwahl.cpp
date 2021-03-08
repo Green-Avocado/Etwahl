59163de37d1c489d866745f4d7644baaed4507d8
Green-Avocado <greenavocado@protonmail.com>
2021-03-07 17:17:38 -0800

    feat: added template midi input source

--- src/etwahl.cpp
+++ src/etwahl.cpp
@@ -1,15 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+#include <signal.h>
 #include "RtMidi.h"
 
-int main() {
-    try
-    {
-        RtMidiIn midiin;
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+bool done;
+static void finish(int ignore){ done = true; }
+
+int main()
+{
+    RtMidiIn *midiin = new RtMidiIn();
+    std::vector<unsigned char> message;
+    int nBytes, i;
+    double stamp;
+
+    // Check available ports.
+    unsigned int nPorts = midiin->getPortCount();
+    if ( nPorts == 0 ) {
+        std::cout << "No ports available!\n";
+        goto cleanup;
     }
-    catch (RtMidiError &error)
-    {
-        // Handle the exception here
-        error.printMessage();
+
+    midiin->openPort( 0 );
+
+    // Don't ignore sysex, timing, or active sensing messages.
+    midiin->ignoreTypes( false, false, false );
+
+    // Install an interrupt handler function.
+    done = false;
+    (void) signal(SIGINT, finish);
+
+    // Periodically check input queue.
+    std::cout << "Reading MIDI from port ... quit with Ctrl-C.\n";
+    while ( !done ) {
+        stamp = midiin->getMessage( &message );
+        nBytes = message.size();
+
+        for ( i=0; i<nBytes; i++ )
+            std::cout << "Byte " << i << " = " << (int)message[i] << ", ";
+
+        if ( nBytes > 0 )
+            std::cout << "stamp = " << stamp << std::endl;
+
+        // Sleep for 10 milliseconds ... platform-dependent.
+        sleep( 0.01 );
     }
+
+    // Clean up
+    cleanup:
+        delete midiin;
 
     return 0;
 }
