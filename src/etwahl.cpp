5e638cc792282cbafbfdcffb0451969e84618dc0
Green-Avocado <greenavocado@protonmail.com>
2021-03-08 01:13:59 -0800

    feat: user selected midi port, style

--- src/etwahl.cpp
+++ src/etwahl.cpp
@@ -13,35 +13,50 @@ int main()
 {
     RtMidiIn *midiin = new RtMidiIn();
     std::vector<unsigned char> message;
-    int nBytes, i;
+    int portNum;
+    int nBytes;
     double stamp;
 
-    // Check available ports.
+    // Check inputs.
     unsigned int nPorts = midiin->getPortCount();
-    if ( nPorts == 0 ) {
-        std::cout << "No ports available!\n";
-        goto cleanup;
+    std::cout << "\nThere are " << nPorts << " MIDI input sources available.\n";
+    std::string portName;
+
+    for (int i=0; i < nPorts; i++) {
+        try {
+            portName = midiin->getPortName(i);
+        }
+        catch ( RtMidiError &error ) {
+            error.printMessage();
+            goto cleanup;
+        }
+        std::cout << "  Input Port #" << i+1 << ": " << portName << '\n';
     }
 
-    midiin->openPort( 0 );
+    // Get port number.
+    std::cin >> portNum;
+    portNum--;
+
+    midiin->openPort(portNum);
 
     // Don't ignore sysex, timing, or active sensing messages.
-    midiin->ignoreTypes( false, false, false );
+    midiin->ignoreTypes(false, false, false);
 
     // Install an interrupt handler function.
     done = false;
     (void) signal(SIGINT, finish);
 
     // Periodically check input queue.
-    std::cout << "Reading MIDI from port ... quit with Ctrl-C.\n";
-    while ( !done ) {
-        stamp = midiin->getMessage( &message );
+    std::cout << "Reading MIDI from port " << portNum + 1 << "... quit with Ctrl-C.\n";
+
+    while(!done) {
+        stamp = midiin->getMessage(&message);
         nBytes = message.size();
 
-        for ( i=0; i<nBytes; i++ )
+        for(int i = 0; i < nBytes; i++)
             std::cout << "Byte " << i << " = " << (int)message[i] << ", ";
 
-        if ( nBytes > 0 )
+        if(nBytes > 0)
             std::cout << "stamp = " << stamp << std::endl;
 
         // Sleep for 10 milliseconds ... platform-dependent.
