9e199409e21b903596af390b4987ca0f52847906
Green-Avocado <greenavocado@protonmail.com>
2021-03-08 19:25:03 -0800

    feat: added template file reader

--- src/config.cpp
+++ src/config.cpp
@@ -1,9 +1,30 @@
 #include "config.h"
+#include <fstream>
+#include <iostream>
 #include <vector>
 
 std::vector<keybind> loadConfig(char* filename)
 {
+    std::string line;
     std::vector<keybind> config;
+    std::ifstream configFile;
+
+    configFile.open(filename);
+
+    if(configFile.is_open())
+    {
+        while(getline(configFile, line))
+        {
+            std::cout << line << std::endl;
+        }
+
+        configFile.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file." << std::endl;
+    }
+
     return config;
 }
 
