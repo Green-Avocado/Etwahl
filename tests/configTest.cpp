5fdd6e6cc11ce7002122985cfd49ed69551f08e3
Green-Avocado <greenavocado@protonmail.com>
2021-03-09 14:16:19 -0800

    feat: added base config test

---
+++ tests/configTest.cpp
@@ -0,0 +1,44 @@
+#include "config.h"
+
+#include "catch.hpp"
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdio>
+
+std::vector<keybind> testConfigFile(std::string filename, std::string contents)
+{
+    std::ofstream testfile;
+    std::vector<keybind> config;
+
+    testfile.open(filename);
+
+    if(testfile.is_open())
+    {
+        testfile << contents;
+        testfile.close();
+    }
+    else
+    {
+        throw;
+    }
+
+    config = loadConfig(filename);
+    
+    std::remove(filename.c_str());
+
+    return config;
+}
+
+std::vector<keybind> testConfigVector()
+{
+    std::vector<keybind> vec;
+    return vec;
+}
+
+TEST_CASE("Configuration file read and interpreted", "[config]")
+{
+    REQUIRE(testConfigFile("test1.config~", "") == testConfigVector());
+}
+
