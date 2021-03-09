0cdeaa92c71d9579fd04bea9487664626b7ae5bd
Green-Avocado <greenavocado@protonmail.com>
2021-03-08 18:36:39 -0800

    feat: include header in implementation

--- src/Linux/keypress.cpp
+++ src/Linux/keypress.cpp
@@ -1,3 +1,4 @@
+#include "keypress.h"
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <X11/extensions/XTest.h>
--- src/Windows/keypress.cpp
+++ src/Windows/keypress.cpp
@@ -1,3 +1,4 @@
+#include "keypress.h"
 #include <winuser.h>
 
 void key(const char *keystring, bool keydown)
